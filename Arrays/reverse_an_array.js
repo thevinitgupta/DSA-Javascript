412600ecf1fdb7042d23aa5c17d4295bc09dbda5
Vinit Gupta <vinitman35@gmail.com>
2021-01-24 18:14:24 +0530

    Used Class Based Static method and 2-pointer approach for Q-1

--- Arrays/reverse_an_array.js
+++ Arrays/reverse_an_array.js
@@ -1,4 +1,4 @@
-const reverseArray = function(arr){
+const reverseArrayFunc = function(arr){
     const len = arr.length;
     if(len===0) return undefined;
     else if(len===1) return arr;
@@ -14,6 +14,28 @@ const reverseArray = function(arr){
         return arr;
     }
 }
-console.log(reverseArray(["Orange","Apple","Grapes"])); //output -> [ 'Grapes', 'Apple', 'Orange' ]
-console.log(reverseArray([4, 5, 1, 2]));//output -> [ 2, 1, 5, 4 ]
-console.log(reverseArray([4, 5, 1, 2,11,25,4]));//output -> [ 4, 25, 11, 2, 1 , 5, 4]
+
+//two pointer approach 
+class Reverse {
+    constructor(_arr){
+        this.arr = _arr;
+    }
+
+    //static reversing method
+    static reverseArray(arr){
+        let start = 0;
+        let end = arr.length -1;
+        while(start!==end){
+            let temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+        return arr;
+    }
+}
+
+console.log(Reverse.reverseArray(["Orange","Apple","Grapes"])); //output -> [ 'Grapes', 'Apple', 'Orange' ]
+console.log(reverseArrayFunc([4, 5, 1, 2]) );//output -> [ 2, 1, 5, 4 ]
+console.log(Reverse.reverseArray([4, 5, 1, 2,11,25,4]));//output -> [ 4, 25, 11, 2, 1 , 5, 4]
