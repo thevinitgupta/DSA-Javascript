00d3126a46f0cd692c582e039515bcb08d95e4ed
Vinit Gupta <vinitman35@gmail.com>
2021-01-26 20:06:20 +0530

    Added two pointer approach to Q-5

--- Arrays/move-negative-to-one-side.js
+++ Arrays/move-negative-to-one-side.js
@@ -11,4 +11,35 @@ const moveNegative = function(arr){
     }
     return arr;
 }
-console.log(moveNegative([-12, 11, -13, -5, 6, -7, 5, -3, -6]));
+
+class Negative {
+    static moveToBeginning(arr){
+        let start = 0;
+        let end = arr.length - 1;
+        while(start<=end){
+            if(arr[start]>=0 && arr[end]>0){
+                end--;
+            }
+            else if(arr[start]>=0 && arr[end]<0){
+                let temp = arr[start];
+                arr[start] = arr[end];
+                arr[end] = temp;
+                start++;
+                end--;
+            }
+            else if(arr[start]<0 && arr[end]<0){
+                start++;
+            }
+            else{
+                start++;
+                end--;
+            }
+            
+        }
+        return arr;
+    }
+}
+console.log(moveNegative([-12, 11, -13, -5, 6, -7, 5, -3, -6]));//[-12, -13, -5, -7, -3,-6,  11,  6,  5]
+console.log(Negative.moveToBeginning([-12, 11, -13, -5, 6, -7, 5, -3,11, -6]));//[-12, -6, -13, -5, -3,-7,  5,   6, 11, 11]
+console.log(Negative.moveToBeginning([]));//[]
+console.log(Negative.moveToBeginning([0,-1,0,-10]));//[ -10, -1, 0, 0 ]
