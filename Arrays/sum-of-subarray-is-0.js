808a934825645a55c803e1c15a98dd891bf4b96d
Vinit Gupta <vinitman35@gmail.com>
2021-02-06 20:37:01 +0530

    Added simple negation solution to Q-21

--- Arrays/sum-of-subarray-is-0.js
+++ Arrays/sum-of-subarray-is-0.js
@@ -1,15 +1,17 @@
 class FindSubArray{
-    static withSumZero(arr,n){
-        let sumTillHere = 0;
-        let prevSum = Number.MIN_VALUE;
-        let start = 0;
-        for(let end=0;end<n-1;end++){
-            sumTillHere += arr[end];
-            if(sumTillHere===0) return true;
-            if(sumTillHere<prevSum){
-
-            }
-            prevSum = sumTillHere;
+    static withSumZero(n,arr){
+        if(arr.indexOf(0)!==-1) return true;
+        let sum = 0;
+        let sumArr = [];
+        for(let i=0;i<n;i++){
+            sum = sum+arr[i];
+            if(sumArr.indexOf(sum)!==-1||sum===0) return true;
+            else sumArr.push(sum);
         }
+        return false;
     }
 }
+
+console.log(FindSubArray.withSumZero(5,[4,4 ,-3, 1, 6]));//output -> false
+console.log(FindSubArray.withSumZero(2,[10,-10]));//output -> true
+console.log(FindSubArray.withSumZero(5,[4 ,2 ,-3, 1, 6]));//output -> true
