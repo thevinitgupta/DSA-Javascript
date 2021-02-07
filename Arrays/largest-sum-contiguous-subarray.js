38a42d65f62231b560d90a7afc21acab7ea4bcae
Vinit Gupta <vinitman35@gmail.com>
2021-02-07 19:25:27 +0530

    Updated solution for Q-8 to account for negative maximum values too

--- Arrays/largest-sum-contiguous-subarray.js
+++ Arrays/largest-sum-contiguous-subarray.js
@@ -1,12 +1,15 @@
 class LargetSumContiguousSubArray {
-    static Kadane(arr){
-        let currSum = 0;
-        let max = 0;
-        for(let i =0;i<arr.length;i++){
-            currSum = currSum+arr[i];
-            if(currSum<0) currSum = 0;
-            if(max<currSum) max = currSum;
+    static kadane(nums){
+        let currSum = nums[0];
+        let max = nums[0];
+        for(let i =1;i<nums.length;i++){
+            currSum = currSum+nums[i]>nums[i] ? currSum+nums[i] : nums[i];
+            max = max<currSum ? currSum : max;
         }
         return max;
     }
 }
+
+
+console.log(LargetSumContiguousSubArray.kadane([-2,1,-3,4,-1,2,1,-5,4]));
+console.log(LargetSumContiguousSubArray.kadane([-100000]));
