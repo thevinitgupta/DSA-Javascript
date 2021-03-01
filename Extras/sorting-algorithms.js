49a2f53f69e5844519640390e8cda54cba93aa71
Vinit Gupta <vinitman35@gmail.com>
2021-03-01 10:28:50 +0530

    Added sort in increasing or decreasing functionality for merge sort

--- Extras/sorting-algorithms.js
+++ Extras/sorting-algorithms.js
@@ -4,25 +4,50 @@ class Sort {
     Sort(_arr){
         this.arr = _arr;
     }
-    static mergeSort(arr){
+    static mergeSort(arr,order){
       let len = arr.length;
       if(len<=1) return arr;
       let mid = Math.floor(len/2);
       let left = arr.slice(0,mid);
       let right = arr.slice(mid,len);
+      if(order==="i"){
+        return Sort.mergeIncrease(
+            Sort.mergeSort(left,"i"),
+            Sort.mergeSort(right,"i")
+          );
+      }
+      else if(order==="d") {
+        return Sort.mergeDecrease(
+            Sort.mergeSort(left,"d"),
+            Sort.mergeSort(right,"d")
+          );
+      }
       
-      return Sort.merge(
-        Sort.mergeSort(left),
-        Sort.mergeSort(right)
-      );
     }
-    static merge(leftArr,rightArr){
+    static mergeIncrease(leftArr,rightArr){
         const lLen = leftArr.length;
         const rLen = rightArr.length;
         let i=0,j=0;
         let merged = [];
         while(i<lLen&&j<rLen){
             if(leftArr[i]<rightArr[j]){
+                merged.push(leftArr[i]);
+                i++;
+            }
+           else {
+                merged.push(rightArr[j]);
+                j++;
+            }
+        }
+        return [...merged,...leftArr.slice(i,lLen),...rightArr.slice(j,rLen)];
+    }
+    static mergeDecrease(leftArr,rightArr){
+        const lLen = leftArr.length;
+        const rLen = rightArr.length;
+        let i=0,j=0;
+        let merged = [];
+        while(i<lLen&&j<rLen){
+            if(leftArr[i]>rightArr[j]){
                 merged.push(leftArr[i]);
                 i++;
             }
@@ -93,3 +118,5 @@ module.exports = Sort;
 
 // console.log(Sort.quickSort([2,7,1,3,9,1]))
 // console.log(Sort.quickSort([17,12,11,6,3]))
+console.log(Sort.mergeSort([2,7,1,3,9,1],"d"));
+console.log(Sort.mergeSort([2,7,1,3,9,1],"i"));
