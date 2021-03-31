b7bd886a2e7aed9da90792be318f71c99d5aa72c
Vinit Gupta <vinitman35@gmail.com>
2021-03-31 11:07:16 +0530

    Added more display and printMatrix functionality

--- matrices/find-specific-pair-in-matrix.js
+++ matrices/find-specific-pair-in-matrix.js
@@ -1,3 +1,5 @@
+const process = require("process");
+
 class FindPair{
     static inMatrix(N,mat){
         //stores maximum value
@@ -15,18 +17,23 @@ class FindPair{
         let maxv = mat[N-1][N-1];  // Initialize max
         for (let j = N - 2; j >= 0; j--)
         {
+            console.log("Prev Max ->",maxv);
             if (mat[N-1][j] > maxv)
                 maxv = mat[N - 1][j];
+            console.log("Updated Max ->",maxv);
             maxArr[N-1][j] = maxv;
+            FindPair.printMatrix(maxArr,N,N);
         }
         
         // preprocess last column
         maxv = mat[N - 1][N - 1];  // Initialize max
         for (let i = N - 2; i >= 0; i--)
-        {
+        {   console.log("Prev Max ->",maxv);
             if (mat[i][N - 1] > maxv)
                 maxv = mat[i][N - 1];
+            console.log("Updated Max ->",maxv);
             maxArr[i][N - 1] = maxv;
+            FindPair.printMatrix(maxArr,N,N);
         }
         
         // preprocess rest of the matrix from bottom
@@ -37,15 +44,24 @@ class FindPair{
                 // Update maxValue
                 if (maxArr[i+1][j+1] - mat[i][j] > maxValue)
                     maxValue = maxArr[i + 1][j + 1] - mat[i][j];
-        
+                console.log("Max Value -> ",maxValue);
                 // set maxArr (i, j)
                 maxArr[i][j] = Math.max(mat[i][j],
                                    Math.max(maxArr[i][j + 1],
                                        maxArr[i + 1][j]) );
+                FindPair.printMatrix(maxArr,N,N);
             }
         }
          
         return maxValue;
+    }
+    static printMatrix(mat,n,m){
+        for(let i=0;i<n;i++){
+            for(let j=0;j<m;j++){
+                process.stdout.write(mat[i][j]+" ");
+            }
+            process.stdout.write("\n");
+        }
     }
 }
 
