515026885fd55344d2f9df9e712acde97595050c
Vinit Gupta <vinitman35@gmail.com>
2021-04-11 12:56:03 +0530

    Add topper solution to Q-2 of LCode Contest-236

--- contest/find-winner-of-circular-game.js
+++ contest/find-winner-of-circular-game.js
@@ -1,23 +1,37 @@
-function findTheWinner(n, k) {
-    let nums = []
-    for(let i=1;i<=n;i++){
-        nums[i-1]=i;
+// function findTheWinner(n, k) {
+//     let nums = []
+//     for(let i=1;i<=n;i++){
+//         nums[i-1]=i;
+//     }
+//     console.log(nums);
+//     let head = 0;
+//     let count =0;
+//     while(nums.length!==1){
+//         console.log(nums)
+//         count = head + k-1;
+//         while(count>=nums.length){
+//             console.log("Before ->",count)
+//             count = count - nums.length;
+//             console.log(count)
+//         }
+//         nums.splice(count,1);
+//         head = count;
+//     }
+//     console.log(nums[0]);
+// }
+
+function findTheWinner(n,k){
+    let friends = []
+    for(let i=0;i<n;i++){
+        friends[i]=i+1;
     }
-    console.log(nums);
-    let head = 0;
-    let count =0;
-    while(nums.length!==1){
-        console.log(nums)
-        count = head + k-1;
-        while(count>=nums.length){
-            console.log("Before ->",count)
-            count = count - nums.length;
-            console.log(count)
+       let current = 0
+        for (let j=0;j<n-1;j++){
+            current =Math.floor((current+k-1)%(n-j));
+            friends.splice(current,1)
+            console.log(friends)
         }
-        nums.splice(count,1);
-        head = count;
-    }
-    console.log(nums[0]);
+        return friends[0]
 }
 
 console.log(findTheWinner(5,2))
