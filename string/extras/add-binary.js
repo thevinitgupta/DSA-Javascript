3587b7d7178299d53ba8c49cef0b4c8d59c6f692
Vinit Gupta <vinitman35@gmail.com>
2021-04-14 21:07:45 +0530

    Add O(N) time solution to leetcode Q-67 add binary

--- string/extras/add-binary.js
+++ string/extras/add-binary.js
@@ -3,51 +3,53 @@
  */
 
 class Solution {
-    // static addBinary(a,b){
-    //     let i=a.length-1,j=b.length-1;
-    //     let tot = "",carry=0;
-    //     while(i>=0 || j>=0){
-    //         console.log("For ",a[i]," + ",b[j])
-    //         let aval = i>=0?a[i] : 0;
-    //         let bval = j>=0?b[j] : 0;
-    //         let currSum = Solution.binarySum([aval,bval,carry]);
-            
-    //         tot = currSum[currSum.length-1]+""+tot;
-    //         carry = currSum.length>1?parseInt(currSum[0]) : 0;
-    //         console.log("Curr Sum ->",currSum,",Carry ->",carry);
-    //         i--;j--;
-    //     }
-    //     if(carry!==0) tot = carry + tot;
-    //     return tot;
-    //}
+
 
     //?works -> faster than 50%
-    static addBinary(a,b){
-        a=a.length>=1 ? Solution.toInteger(a,2) : 0;
-        b=b.length>=1 ? Solution.toInteger(b,2) : 0;
-        let sum = a+b;
-        if(sum==0) return "0";
+    // static addBinary(a,b){
+    //     a=a.length>=1 ? Solution.toInteger(a,2) : 0;
+    //     b=b.length>=1 ? Solution.toInteger(b,2) : 0;
+    //     let sum = a+b;
+    //     if(sum==0) return "0";
 
-        let bin = Solution.binarySum(sum)+"";
-        return bin.slice(0,bin.length-1);
-    }
-    static binarySum(num){
-        let bin = 0n;
-        while(num>0){
-            let dig = BigInt(num%2n);
-            bin = dig + ""+bin;
-            num = BigInt(num/2n);
+    //     let bin = Solution.binarySum(sum)+"";
+    //     return bin.slice(0,bin.length-1);
+    // }
+    // static binarySum(num){
+    //     let bin = 0n;
+    //     while(num>0){
+    //         let dig = BigInt(num%2n);
+    //         bin = dig + ""+bin;
+    //         num = BigInt(num/2n);
+    //     }
+    //     return bin;
+    // }
+    // static toInteger(s){
+    //     let num = 0n,pow=0;
+    //     for(let i=s.length-1;i>=0;i--){
+    //         num = num+ BigInt(s[i]*(2**pow));
+    //         console.log(num);
+    //         pow++; 
+    //     }
+    //     return num;
+    // }
+
+    //*Fastest O(Math.max(a.length,b.length)) ie O(N) time
+    static addBinary(a,b){
+        let sumStr = "";
+        let i = a.length - 1;
+        let j = b.length - 1;
+        let carry = 0;
+        while(i>=0 || j>=0){
+            let sum = carry;
+            sum += Number(a[i]) || 0;
+            sum += Number(b[j]) || 0;
+            sumStr = (sum%2)+""+sumStr;
+            carry = parseInt(sum/2);
+            i--;j--;
         }
-        return bin;
-    }
-    static toInteger(s){
-        let num = 0n,pow=0;
-        for(let i=s.length-1;i>=0;i--){
-            num = num+ BigInt(s[i]*(2**pow));
-            console.log(num);
-            pow++; 
-        }
-        return num;
+        if(carry!==0) sumStr = carry+""+sumStr;
+        return sumStr;
     }
 }
 
