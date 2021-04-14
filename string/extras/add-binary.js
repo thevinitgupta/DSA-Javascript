466f5c54fca1f59b13da5fab765aee76ca5d2b61
Vinit Gupta <vinitman35@gmail.com>
2021-04-14 20:35:14 +0530

    Add working solution for leetcode Q-67 add binary using BigInt

--- string/extras/add-binary.js
+++ string/extras/add-binary.js
@@ -20,28 +20,30 @@ class Solution {
     //     if(carry!==0) tot = carry + tot;
     //     return tot;
     //}
+
+    //?works -> faster than 50%
     static addBinary(a,b){
-        a=a.length>=1 ? parseInt(a,2) : 0;
-        b=b.length>=1 ?parseInt(b,2) : 0;
+        a=a.length>=1 ? Solution.toInteger(a,2) : 0;
+        b=b.length>=1 ? Solution.toInteger(b,2) : 0;
         let sum = a+b;
         if(sum==0) return "0";
 
-        let bin = Solution.binarySum(sum)
-        return bin;
+        let bin = Solution.binarySum(sum)+"";
+        return bin.slice(0,bin.length-1);
     }
     static binarySum(num){
-        let bin = "";
+        let bin = 0n;
         while(num>0){
-            let dig = num%2;
+            let dig = BigInt(num%2n);
             bin = dig + ""+bin;
-            num = parseInt(num/2);
+            num = BigInt(num/2n);
         }
         return bin;
     }
     static toInteger(s){
-        let num = 0,pow=0;
+        let num = 0n,pow=0;
         for(let i=s.length-1;i>=0;i--){
-            num = num+ s[i]*(2**pow);
+            num = num+ BigInt(s[i]*(2**pow));
             console.log(num);
             pow++; 
         }
@@ -50,4 +52,5 @@ class Solution {
 }
 
 
-console.log(Solution.toInteger("110101001011101110001111100110001010100001101011101010000011011011001011101111001100000011011110011"));
+console.log(Solution.addBinary("10100000100100110110010000010101111011011001101110111111111101000000101111001110001111100001101",
+"110101001011101110001111100110001010100001101011101010000011011011001011101111001100000011011110011"))//returns 110111101100010011000101110110100000011101000101011001000011011000001100011110011010010011000000000
