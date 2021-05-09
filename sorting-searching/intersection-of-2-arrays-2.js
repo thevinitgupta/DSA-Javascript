bb6307d4d6c0d164924e40db64ecdac3c0c11273
Vinit Gupta <vinitman35@gmail.com>
2021-05-09 21:37:35 +0530

    add solution to leetcode Q-350 intersection of 2 arrays-II

--- sorting-searching/intersection-of-2-arrays-2.js
+++ sorting-searching/intersection-of-2-arrays-2.js
@@ -10,29 +10,29 @@ console.log(intersect([4,9,5],[9,4,9,8,4]))
  * @param {number[]} nums2
  * @return {number[]}
  */
+
+//?faster than 80%, more efficient than 60%
  var intersect = function(nums1, nums2) {
-     let concat = [...nums1,...nums2];
-    concat.sort((a,b)=>a-b);
+     if(nums2.length<nums1.length) return intersect(nums2,nums1);
     let reps = {};
-    for(let i=0;i<concat.length;i++){
-        if(reps[concat[i]]) reps[concat[i]]++;
-        else reps[concat[i]] = 1;
+    for(let i=0;i<nums1.length;i++){
+        if(reps[nums1[i]]) reps[nums1[i]]++;
+        else reps[nums1[i]] = 1;
     }
-    concat = [];
     console.log(reps);
-    for (const prop in reps) {
-        if(reps[prop]>=2){
-            console.log(reps[prop])
-            let lim = reps[prop]%2===0 ? reps[prop]/2: (reps[prop]-1)/2;
-            console.log("limit-> ",lim);
-                for(let i=1;i<=lim;i++){
-                    concat.push(parseInt(prop));
-                }
+    let intersec = [];
+    for(let i=0;i<nums2.length;i++){
+        let count = reps[nums2[i]] || 0;
+        if(count>0){
+            intersec.push(nums2[i]);
+            reps[nums2[i]] -= 1;
         }
-        
     }
-   return concat; 
+
+    return intersec;
 };
 
 
 console.log(intersect([9],[9]))
+console.log(intersect([1,2,2,1],[2,2]))
+console.log(intersect([4,9,5],[9,4,9,8,4]))
