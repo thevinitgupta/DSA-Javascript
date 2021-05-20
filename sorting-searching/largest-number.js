72548f6f259e21ee91f842e7286018d7b04e2966
Vinit Gupta <vinitman35@gmail.com>
2021-05-20 20:12:05 +0530

    add context to solution to leetcode Q-179 largest number

--- sorting-searching/largest-number.js
+++ sorting-searching/largest-number.js
@@ -1,4 +1,14 @@
 /**
+ * Given a list of non-negative integers nums, arrange them such that they form the largest number.
+Note: The result may be very large, so you need to return a string instead of an integer.
+
+Example 1:
+Input: nums = [10,2]
+Output: "210"
+ */
+
+
+/**
  * @param {number[]} nums
  * @return {string}
  */
