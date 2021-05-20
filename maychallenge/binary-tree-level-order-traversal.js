2d65c1eb5a2da094825644fdcc2eab55f4da3155
Vinit Gupta <vinitman35@gmail.com>
2021-05-20 19:28:32 +0530

    add solution to May leetcoding challenge Day-20 binary tree level order traversal

---
+++ maychallenge/binary-tree-level-order-traversal.js
@@ -0,0 +1,30 @@
+/**
+ ** Given the root of a binary tree, return the level order traversal of its nodes' values. (i.e., from left to right, level by level).
+ */
+
+ /**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrder = function(root) {
+    let q = [root], ans = []
+    while (q[0]) {
+        let qlen = q.length, row = []
+        for (let i = 0; i < qlen; i++) {
+            let curr = q.shift()
+            row.push(curr.val)
+            if (curr.left) q.push(curr.left)
+            if (curr.right) q.push(curr.right)
+        }
+        ans.push(row)            
+    }
+    return ans
+};
