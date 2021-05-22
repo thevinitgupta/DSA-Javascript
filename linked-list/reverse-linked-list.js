081b5978bb669f3d3d09112f1900eec84f779502
Vinit Gupta <vinitman35@gmail.com>
2021-05-22 20:49:34 +0530

    add solution to leetcode Q-206 reverse linked list

---
+++ linked-list/reverse-linked-list.js
@@ -0,0 +1,30 @@
+/**
+ * Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+ 
+ */
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+
+//? faster than 29% and memory efficient more than 95%g
+ var reverseList = function(head) {
+    let prev = null,next;
+    while(head!==null){
+        next = head.next;
+        head.next = prev;
+        prev = head;
+        head = next;
+    }
+    head = prev;
+    return head;
+};
