5182c6deb32484ff0510f9c5464489dcf72ce8c4
Vinit Gupta <vinitman35@gmail.com>
2021-05-24 17:14:39 +0530

    add solution to May leetcoding challenge day-24 to lower case

---
+++ maychallenge/to-lower-case.js
@@ -0,0 +1,9 @@
+//Given a string s, return the string after replacing every uppercase letter with the same lowercase letter.
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+ var toLowerCase = function(s) {
+    return s.toLowerCase();
+};
