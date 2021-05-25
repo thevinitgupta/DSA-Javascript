1db0f46cc95535cbb48a88950492f173cb7081d7
Vinit Gupta <vinitman35@gmail.com>
2021-05-25 20:09:45 +0530

    add O(mxn) solution to leetcode Q-1669 merge in between linked lists

---
+++ linked-list/merge-in-between-lists.js
@@ -0,0 +1,49 @@
+/**
+ * You are given two linked lists: list1 and list2 of sizes n and m respectively.
+
+Remove list1's nodes from the ath node to the bth node, and put list2 in their place.
+Build the result list and return its head.
+
+Example 1:
+Input: list1 = [0,1,2,3,4,5], a = 3, b = 4, list2 = [1000000,1000001,1000002]
+Output: [0,1,2,1000000,1000001,1000002,5]
+Explanation: We remove the nodes 3 and 4 and put the entire list2 in their place. The blue edges and nodes in the above figure indicate the result.
+ */
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {number} a
+ * @param {number} b
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+ var mergeInBetween = function(list1, a, b, list2) {
+    let left=list1,right=list2,curr = list1;
+    let c1=0,c2=0;
+    while(curr){
+        if(c1+1===a){
+            left = curr.next;
+            curr.next = list2;
+            while(curr.next!==null){
+                curr=curr.next;
+            }
+            curr.next = left;
+            c1++;
+            while(c1<b){
+                    left = left.next;
+                    c1++;
+                  }
+            curr.next = left.next;
+            return list1;
+        }
+        curr = curr.next;
+        c1++;
+    }
+};
