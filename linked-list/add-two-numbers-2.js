cf777382f415684f85bad8b2cf2808844b97b405
Vinit Gupta <vinitman35@gmail.com>
2021-05-29 20:43:00 +0530

    add O(max(M,N)) solutiont to leetcode Q-445 add two numbers - part 2

--- linked-list/add-two-numbers-2.js
+++ linked-list/add-two-numbers-2.js
@@ -41,3 +41,44 @@ You may assume the two numbers do not contain any leading zero, except the numbe
     }
     return head.next;
 };
+
+//* faster than 54% -> O(max(M,N)) time
+var addTwoNumbers = function(l1, l2) {
+    let num1 = [];
+    let num2 = [];
+    if(!l1) return l2;
+    if(!l2) return l1;
+    while(l1){
+        num1.push(l1.val);
+        l1 = l1.next;
+    }
+    while(l2){
+        num2.push(l2.val);
+        l2 = l2.next;
+    }
+    let sumStack = [];
+    let head = new ListNode(0);
+    let l3 = head;
+    let carry = 0;
+    while(num1.length>0 || num2.length>0){
+        let v1 = num1.length>0? num1.pop() : 0;
+        let v2 = num2.length? num2.pop() : 0;
+        let sum = v1+v2+carry;
+        carry=0;
+        let curr = sum;
+        if(sum>9){
+            curr = sum%10;
+            carry = 1; 
+        }
+        sumStack.push(curr);
+    }
+    if(carry>0){
+        sumStack.push(carry);
+    }
+    while(sumStack.length>0){
+        let newNode = new ListNode(sumStack.pop());
+        l3.next = newNode;
+        l3 = l3.next;
+    }
+    return head.next;
+};
