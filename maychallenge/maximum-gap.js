b9da8d8aa629d05b3eaf5d92fca64a5deeef384a
Vinit Gupta <vinitman35@gmail.com>
2021-05-30 22:45:34 +0530

    add solution to May leetcoding challenge day 30 maximum gap

--- maychallenge/maximum-gap.js
+++ maychallenge/maximum-gap.js
@@ -3,3 +3,18 @@
 
 You must write an algorithm that runs in linear time and uses linear extra space.
  */
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+ var maximumGap = function(nums) {
+    if(nums.length<=1) return 0;
+    nums.sort((a,b)=>a-b);
+    let max = 0,diff = 0;
+    for(let i=1;i<nums.length;i++){
+        diff = nums[i]-nums[i-1];
+        max = Math.max(max,diff);
+    }
+    return max;
+};
