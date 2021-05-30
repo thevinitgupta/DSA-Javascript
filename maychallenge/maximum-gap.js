e4ad9e0436030234fc85b815cce312c00c2dadba
Vinit Gupta <vinitman35@gmail.com>
2021-05-30 22:45:00 +0530

    add question to May leetcoding challenge day 30 maximum gap

---
+++ maychallenge/maximum-gap.js
@@ -0,0 +1,5 @@
+/**
+ * Given an integer array nums, return the maximum difference between two successive elements in its sorted form. If the array contains less than two elements, return 0.
+
+You must write an algorithm that runs in linear time and uses linear extra space.
+ */
